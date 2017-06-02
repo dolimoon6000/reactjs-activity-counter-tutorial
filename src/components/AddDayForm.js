4bea5541f6377ba9fa2e3df61c271c19915bb2cc
Oludolapo Soboyede <dolimoon6000@yahoo.com>
2017-06-02 21:13:13 +0100

    Added submit button and submit function to AddDayForm

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -2,25 +2,45 @@ import { Component } from 'react'
 import { PropTypes } from 'prop-types'
 
 export class AddDayForm extends Component {
+    constructor(props) {
+        super(props)
+        this.submit = this.submit.bind(this)
+    }
+
+    submit(e) {
+        e.preventDefault()
+        console.log('resort', this.refs.resort.value)
+        console.log('date', this.refs.date.value)
+        console.log('powder', this.refs.powder.checked)
+        console.log('backcountry', this.refs.backcountry.checked)
+    }
 
     render() {
         const { resort, date, powder, backcountry } = this.props
         return (
-            <form className="add-day-form">
+            <form onSubmit={this.submit} className="add-day-form">
                 <label htmlFor="resort">Resort Name</label>
-                <input type="text" id="resort" required defaultValue={resort} />
+                <input type="text" id="resort" required defaultValue={resort} ref="resort" />
 
                 <label htmlFor="date">Date</label>
-                <input type="date" id="date" required defaultValue={date} />
+                <input type="date" id="date" required defaultValue={date} ref="date"/>
+
                 <div>
-                    <input type="checkbox" id="powder" defaultChecked={powder} />
+                    <input type="checkbox" id="powder" defaultChecked={powder} ref="powder" />
                     <label htmlFor="powder">Powder Day</label>
                 </div>
 
                 <div>
-                    <input type="checkbox" id="backcountry" defaultChecked={backcountry} />
+                    <input
+                        type="checkbox"
+                        id="backcountry"
+                        defaultChecked={backcountry}
+                        ref="backcountry"
+                    />
                     <label htmlFor="backcountry">Backcountry Day</label>
                 </div>
+
+                <button>Add Day</button>
             </form>
         )
     }
