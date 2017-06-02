fdc19db9cf0cbaa1c5e05296ad4cb4d8fd0b5aa0
Oludolapo Soboyede <dolimoon6000@yahoo.com>
2017-06-02 08:56:44 +0100

    use inline style

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,10 @@
 const { createElement } = React
 const { render } = ReactDOM
 
-const style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'verdana'
-}
-
 render(
-    <h1 id="title" className="header" style={style}>
+    <h1 id="title" className="header" style={{backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'verdana'}}>
         Hello World
     </h1>,
     document.getElementById('react-container')
