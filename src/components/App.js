d1483f3d11231b0ec2e3a81d2e0ae351faa8ec9f
Oludolapo Soboyede <dolimoon6000@yahoo.com>
2017-06-02 15:47:05 +0100

    Working with state in ES6 class

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import { createClass } from 'react'
+import { Component } from 'react'
 import { SkiDayList } from './SkiDayList'
 import { SkiDayCount } from './SkiDayCount'
 
-export const App = createClass({
-    getInitialState() {
-        return {
+export class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
             allSkiDays: [
                 {
                     resort: "Squaw Valley",
@@ -26,10 +28,10 @@ export const App = createClass({
                 }
             ]
         }
-    },
+    }
     countDays(filter) {
         return this.state.allSkiDays.filter(day => filter ? day[filter] : day).length
-    },
+    }
     render() {
         return (
             <div className="app">
@@ -42,4 +44,4 @@ export const App = createClass({
             </div>
         )
     }
-})
+}
